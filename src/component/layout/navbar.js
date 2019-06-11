5964a0d445585d7e68fb4ffe4995b3c96669335d
Parmar <v796344@global.ual.com>
2019-06-11 15:52:59 -0500

    first commit

---
+++ src/component/layout/navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Link} from 'react-router-dom' 
+import SignedInLinks from './SignedIn.js'
+import SignedOutLinks from './signedOut.js'
+
+
+
+const Navbar = () => {
+
+    return(
+    <nav className = "nav-wrapper grey darken-3"> 
+    <div className="container">
+
+    <Link to ='./' className="brand-logo">EmpPlan</Link>
+        <SignedInLinks></SignedInLinks>
+        <SignedOutLinks></SignedOutLinks>
+ 
+
+        </div>
+    </nav>
+    )
+
+
+}
+
+export default Navbar
